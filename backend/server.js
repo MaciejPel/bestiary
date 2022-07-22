55edbcfbd178b625d59ef435866d295575d79ad8
Maciej Pełczyński <maciejpelczynski0@gmail.com>
2022-07-22 02:48:41 +0200

    deploy changes pt.6 test

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,9 @@ const helmet = require('helmet');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const { errorHandler } = require('./middleware/errorMiddleware');
-require('./config/imgurAPI');
+// require('./config/imgurAPI');
 
-connectDB();
+// connectDB();
 const app = express();
 app.use(express.json());
 app.use(fileUpload());
